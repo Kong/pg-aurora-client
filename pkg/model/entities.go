6a176d15c72798a631d4ffd98d83c3b8384d2ec2
rajkong <rajan.mahadevan@konghq.com>
2022-07-27 08:59:47 -0700

    Adding canary

--- pkg/model/entities.go
+++ pkg/model/entities.go
@@ -2,6 +2,8 @@ package model
 
 import (
 	"database/sql"
+	"fmt"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"go.uber.org/zap"
 	"time"
 )
@@ -25,6 +27,8 @@ var replicaStatusQuery = `SELECT SERVER_ID, SESSION_ID, LAST_UPDATE_TIMESTAMP FR
 var getLastFooQuery = `SELECT ID, CREATED_AT, UPDATED_AT FROM FOO ORDER BY CREATED_AT DESC LIMIT 1`
 
 var insertFoo = `INSERT INTO foo (id) VALUES (default)`
+
+var updateHealthQuery = `UPDATE canary SET id=id +1, ts = CURRENT_TIMESTAMP`
 
 type Store struct {
 	DB     *sql.DB
@@ -100,6 +104,19 @@ func (s *Store) InsertFoo() (int64, error) {
 	return affected, nil
 }
 
+func (s *Store) UpdateCanary() (int64, error) {
+	exec, err := s.DB.Exec(updateHealthQuery)
+	var affected int64
+	if err != nil {
+		return affected, err
+	}
+	affected, err = exec.RowsAffected()
+	if err != nil {
+		return affected, err
+	}
+	return affected, nil
+}
+
 func (s *Store) GetConnectionPoolStats() sql.DBStats {
 	stat := s.DB.Stats()
 	return stat
@@ -108,4 +125,73 @@ func (s *Store) GetConnectionPoolStats() sql.DBStats {
 func (s *Store) GetROConnectionPoolStats() sql.DBStats {
 	stat := s.RODB.Stats()
 	return stat
+}
+
+func NewStore(logger *zap.Logger) (*Store, error) {
+	pgc, err := loadPostgresConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	var dsn string
+	var rodsn string
+	if !pgc.enableTLS {
+		dsn = fmt.Sprintf(dsnNoTLS, pgc.user, pgc.password, pgc.hostURL, pgc.port, pgc.database)
+		if pgc.roHostURL != "" {
+			rodsn = fmt.Sprintf(dsnNoTLS, pgc.user, pgc.password, pgc.roHostURL, pgc.port, pgc.database)
+		}
+	} else {
+		dsn = fmt.Sprintf(dsnTLS, pgc.user, pgc.password, pgc.hostURL, pgc.port, pgc.database, pgc.caBundleFSPath)
+		if pgc.roHostURL != "" {
+			rodsn = fmt.Sprintf(dsnTLS, pgc.user, pgc.password, pgc.roHostURL, pgc.port, pgc.database,
+				pgc.caBundleFSPath)
+		}
+	}
+	pool, err := openPool(dsn, pgc, logger)
+	if err != nil {
+		return nil, err
+	}
+	logger.Info("Established DB Connection")
+	store := &Store{
+		DB:     pool,
+		Logger: logger,
+	}
+
+	if rodsn != "" {
+		rodbPool, err := openPool(rodsn, pgc, logger)
+		if err != nil {
+			return nil, err
+		}
+
+		store.RODB = rodbPool
+		logger.Info("Established RO DB Connection")
+	}
+	return store, nil
+}
+
+const defaultMaxConnections = 50
+const defaultMinConnections = 20
+
+func openPool(dsn string, pgc *PgConfig, logger *zap.Logger) (*sql.DB, error) {
+	logger.Info("DB connection:", zap.String("host", pgc.hostURL),
+		zap.Bool("Enable TLS", pgc.enableTLS),
+		zap.String("user", pgc.user), zap.String("port", pgc.port),
+		zap.String("database", pgc.database), zap.String("caBundlePath", pgc.caBundleFSPath))
+	config, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	config.MaxConns = defaultMaxConnections
+	config.MinConns = defaultMinConnections
+
+	dbpool, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = dbpool.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return dbpool, nil
 }
